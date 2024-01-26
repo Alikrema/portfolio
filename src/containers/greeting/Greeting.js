4ed92ca8398041e4923430e65be7f2a6684177d2
alikrema <alimk1741@gmail.com>
2024-01-26 04:48:36 +0200

    Revert "Merge branch 'master' of github.com:Alikrema/portfolio"
    This reverts commit 3c82bc93ea9ae5227df2119957d56606505361d3, reversing
    changes made to 0bae630d11c06558520ddd3a3e04a499befa439d.

---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,87 @@
+import React, {useContext} from "react";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import "./Greeting.scss";
+import landingPerson from "../../assets/lottie/landingPerson";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+
+import {illustration, greeting} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+import TextSwitcher from "../../components/TextSwitch/TextSwitch";
+import PhotoGallery from "../../components/swipeable/Swipeable";
+
+export default function Greeting() {
+  const {isDark} = useContext(StyleContext);
+  if (!greeting.displayGreeting) {
+    return null;
+  }
+  return (
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1
+                className={isDark ? "dark-mode greeting-text" : "greeting-text"}
+              >
+                {" "}
+                {greeting.title}{" "}
+              </h1>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle[0]}
+              </p>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle[1]}
+              </p>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle[2]}
+              </p>
+              <SocialMedia />
+              <div className="button-greeting-div">
+                <Button text="Contact me" href="#contact" />
+                {greeting.resumeLink && (
+                  <Button
+                    text="See my resume"
+                    newTab={true}
+                    href={greeting.resumeLink}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+          {/* <div className="greeting-image-div">
+            {illustration.animated ? (
+              <DisplayLottie animationData={landingPerson} />
+            ) : (
+              <img
+                alt="man sitting on table"
+                src={require("../../assets/images/manOnTable.svg")}
+              ></img>
+            )}
+          </div> */}
+          <PhotoGallery images={greeting.gallery} />
+        </div>
+      </div>
+    </Fade>
+  );
+}
