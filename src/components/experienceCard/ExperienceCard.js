e985b849635d92d3a765c1cf54cc3cb67f45a144
alikrema <alimk1741@gmail.com>
2024-01-27 19:43:12 +0200

    Remove onClick from experience card

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -36,10 +36,7 @@ export default function ExperienceCard({cardInfo, isDark}) {
   };
 
   return (
-    <div
-      className={isDark ? "experience-card-dark" : "experience-card"}
-      onClick={handleClick}
-    >
+    <div className={isDark ? "experience-card-dark" : "experience-card"}>
       <div style={{background: rgb(colorArrays)}} className="experience-banner">
         <div className="experience-blurred_div"></div>
         <div className="experience-div-company">
